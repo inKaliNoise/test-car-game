0c445b24797273cd16b7b0a1a4d76cef9ca8611e
Konrad <kooonrad.noise@gmail.com>
2020-07-23 15:10:50 +0000

    upgrade code

--- app.py
+++ app.py
@@ -2,21 +2,19 @@
 
 choose = input(">")
 
-# help list
-if choose.lower() == 'help':
-    print('''
-    start - to start the car
-    stop - to stop the car
-    quit - to exit''')
-    choose = input(">")
-
-# game engine 
+# game engine
 while choose.lower() != 'quit':
     if choose.lower() == 'start':
         print('Car started...Ready to go!')
         choose = input(">")
     elif choose.lower() == 'stop':
         print('Car stopped.')
+        choose = input(">")
+    elif choose.lower() == 'help':
+        print('''
+        start - to start the car
+        stop - to stop the car
+        quit - to exit''')
         choose = input(">")
     else:
         print("I don't understand that...")
