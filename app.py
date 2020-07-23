5d4019a92d9756d3dd6e1d5f96434c554ad74192
Konrad <kooonrad.noise@gmail.com>
2020-07-23 16:04:53 +0000

    upgrade code

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 # car game
 
-choose = input(">")
+choose = ""
+speed = 0
 
 # game engine
-while choose.lower() != 'quit':
-    if choose.lower() == 'start':
+while True:
+    choose = input(">").lower()
+    if choose == 'start' and speed < 29:
         print('Car started...Ready to go!')
-        choose = input(">")
-    elif choose.lower() == 'stop':
-        print('Car stopped.')
-        choose = input(">")
-    elif choose.lower() == 'help':
-        print('''
-        start - to start the car
-        stop - to stop the car
-        quit - to exit''')
-        choose = input(">")
+        speed += 30
+    elif choose == 'start' and speed > 29:
+        speed += 30
+        print(f'the car speed is {speed} km/h')
+        if speed > 329:
+            speed -= 30
+            print("It's maximal speed in this car")
+    elif choose == 'stop':
+        speed -= 70
+        if speed >= 0:
+            print(f'the slowed down to {speed} km/h')
+        if speed < 0:
+            print('Car stopped.')
+    elif choose == 'help':
+        print('''start - to start the car
+stop - to stop the car
+quit - to exit''')
+    elif choose == 'quit':
+        break
     else:
         print("I don't understand that...")
-        choose = input(">")
